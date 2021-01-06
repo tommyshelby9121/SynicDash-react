5cb5cb4f9623c7d01ee982e07bb7c070145d185c
Sycrixx <biometricmc@gmail.com>
2021-01-07 08:21:22 +1100

    Add guild filter & check if user exists & run from there

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -11,6 +11,35 @@ module.exports = (passport:any) => {
         callbackURL: process.env.DISCORD_OAUTH_CALLBACK_URL,
         scope: ['identify', 'guilds'],
     }, async (accessToken:string, refreshToken:string, profile:Profile, done:any) => {
+        const { id, username, discriminator, avatar, guilds } = profile;
+        try {
+            // Filter Guilds where permissions has 0x20
+            const filteredGuilds = guilds?.filter(obj => (obj.permissions & 0x20) === 0x20);
 
-    }))
+            // Check if user exists and update
+            const findUser = await User.findOneAndUpdate({ discordId: id }, {
+                discordTag: `${username}#${discriminator}`,
+                avatar,
+                guilds: filteredGuilds,
+            }, { new: true });
+            // If user exists return findUser
+            if (findUser) {
+                return done(null, findUser);
+            }
+            // If user does not exist, create & return newUser
+            else {
+                const newUser = await User.create({
+                    discordId: id,
+                    discordTag: `${username}#${discriminator}`,
+                    avatar,
+                    guilds: filteredGuilds,
+                });
+                return done(null, newUser);
+            }
+        }
+        catch (err) {
+            console.error(err);
+            done(err, null);
+        }
+    }));
 }
