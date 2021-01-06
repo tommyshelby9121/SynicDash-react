42385fbefba6c8298ae1584b3fa2910b460564b4
Sycrixx <biometricmc@gmail.com>
2021-01-07 08:46:13 +1100

    Wrapped App component with Router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+        <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('app')
 );
