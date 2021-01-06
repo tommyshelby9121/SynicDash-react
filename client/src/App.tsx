95782ac3e01d440ee612c67a677ae4fa3c0be6f8
Sycrixx <biometricmc@gmail.com>
2021-01-07 08:43:44 +1100

    Initial Commit

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component<any, {}> {
+  render(): JSX.Element {
+    return (
+        <h1>Hello World</h1>
+    );
+  }
+}
+
+export default App;
