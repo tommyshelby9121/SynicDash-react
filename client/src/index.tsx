9b0ffb1e736242cdcca4772915d0500025585c05
Sycrixx <biometricmc@gmail.com>
2021-01-07 10:46:43 +1100

    Import scss

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import "./scss/main.scss";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
